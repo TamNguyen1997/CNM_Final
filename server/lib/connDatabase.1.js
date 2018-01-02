807f2b91e2c4944a93ba1fdad693814367a7c519
Tam Nguyen <tam070397@gmail.com>
2018-01-02 19:39:38 +0700

    reformat some file

--- server/lib/connDatabase.1.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require("mongoose");
-// mongoose.Promise = global.Promise;
-
-// //process.env.DATABASE_URL || DATABASE_LOCAL_URL
-// mongoose.connect(process.env.DATABASE_LOCAL_URL, { useNewUrlParser: true })
-// .catch(err => {
-//     console.log(err);
-//     process.exit(1)
-// });
