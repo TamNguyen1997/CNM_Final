c3cc806f17ce6e328e03bb5366a99f431be4d508
VuAnhTai <vuanhtai1997@gmail.com>
2019-01-05 20:14:56 +0700

    change database

--- internet_banking.sql
+++ internet_banking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 30, 2018 at 03:24 PM
+-- Generation Time: Jan 05, 2019 at 02:14 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -43,84 +43,10 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`user_id`, `number`, `balance`, `status`, `type_account`, `createdAt`, `updatedAt`) VALUES
-(123, 1, 12341234, 12341234, 0, '2018-12-29 16:56:01', '2018-12-29 16:56:01'),
-(123, 100000000, 0, 0, 0, '2018-12-29 16:56:01', '2018-12-29 16:56:01'),
-(123, 100000001, 213, 213, 0, '2018-12-29 16:56:01', '2018-12-29 16:56:01'),
+(13, 1, 12341234, 12341234, 1, '2018-12-29 16:56:01', '2018-12-29 16:56:01'),
+(13, 100000000, 0, 0, 2, '2018-12-29 16:56:01', '2018-12-29 16:56:01'),
+(13, 100000001, 213, 213, 3, '2018-12-29 16:56:01', '2018-12-29 16:56:01'),
 (10, 100000002, 123, 0, 0, '2018-12-29 11:07:05', '2018-12-29 11:07:05');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `detail_transaction`
---
-
-CREATE TABLE `detail_transaction` (
-  `id_tranaction` int(11) NOT NULL,
-  `type_balance` int(3) NOT NULL,
-  `amount_money` int(11) NOT NULL,
-  `fee_payment` int(3) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction`
---
-
-CREATE TABLE `transaction` (
-  `id` int(11) NOT NULL,
-  `account_src` int(11) NOT NULL,
-  `account_des` int(11) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `type_account`
---
-
-CREATE TABLE `type_account` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `type` int(3) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name_user` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `date_of_birth` datetime NOT NULL,
-  `address` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `phone` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `status` int(3) NOT NULL,
-  `number_account` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `username`, `password`, `type`, `createdAt`, `updatedAt`, `name_user`, `date_of_birth`, `address`, `phone`, `email`, `status`, `number_account`) VALUES
-(10, 'VAT', '202cb962ac59075b964b07152d234b70', 1, '2018-12-25 15:41:55', '2018-12-25 15:41:55', '0', '0000-00-00 00:00:00', '0', '0', '0', 0, 0),
-(11, 'abc', '123', 0, '2018-12-30 18:33:52', '2018-12-30 18:33:52', '', '0000-00-00 00:00:00', '', '', '', 0, 0),
-(12, 'admin', '$2b$08$7wG3p.cFWN6XvdVcR6Os7eTF1sufY00z9L79ygZBXX4', 0, '2018-12-30 12:07:43', '2018-12-30 12:07:43', 'admin', '0000-00-00 00:00:00', '', '0123456789', 'asdf@gmail.com', 0, 0),
-(13, 'admin2', '21232f297a57a5a743894a0e4a801fc3', 0, '2018-12-30 12:37:22', '2018-12-30 12:37:22', 'admin', '0000-00-00 00:00:00', '', '0123456789', 'asdf@gmail.com', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -133,24 +59,6 @@ ALTER TABLE `account`
   ADD PRIMARY KEY (`number`);
 
 --
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `type_account`
---
-ALTER TABLE `type_account`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -159,24 +67,6 @@ ALTER TABLE `user`
 --
 ALTER TABLE `account`
   MODIFY `number` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100000003;
-
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `type_account`
---
-ALTER TABLE `type_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
