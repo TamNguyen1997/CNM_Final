2cdc07851bb768245c820a3168fdcb3908650871
Tam Nguyen <tam070397@gmail.com>
2019-01-06 22:16:24 +0700

    reformat code, add a few modules

---
+++ server/models/advert.js
@@ -0,0 +1,14 @@
+const sequelize = require("sequelize");
+const CONFIG = require("../config/config.json");
+
+const Sequelize = new sequelize(CONFIG.DB.DATABASE, CONFIG.DB.USERNAME, CONFIG.DB.PASSWORD, {
+    host: CONFIG.DB.HOST,
+    dialect: CONFIG.DB.DIALECT,
+
+    pool: {
+        max: 5,         //number of min connecion
+        min: 0,         //number of min connecion
+    }
+});
+
+module.exports = Sequelize;
